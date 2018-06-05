678b2d96459f4c65f9f611141bfde3b6330a6bde
ItsOnlyBinary <github@itsonlybinary.com>
2018-06-05 13:18:25 +0100

    be more specific on Mask test of ToLine()

--- pkg/irc/message.go
+++ pkg/irc/message.go
@@ -44,7 +44,7 @@ func (m *Message) ToLine() string {
 		}
 	}
 
-	if *m.Prefix != (Mask{}) {
+	if m.Prefix.Nick != "" || m.Prefix.Username != "" || m.Prefix.Hostname != "" {
 		prefix := ""
 
 		if m.Prefix.Nick != "" {
