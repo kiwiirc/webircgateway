6fcd28bf5d5d405192bb57a6e24f75caa9826678
Darren <darren@darrenwhitlen.com>
2018-09-01 21:46:22 +0100

    Moving general functions to utils.go

---
+++ pkg/webircgateway/utils.go
@@ -0,0 +1,54 @@
+package webircgateway
+
+import (
+	"strings"
+	"fmt"
+	"unicode/utf8"
+	"golang.org/x/net/html/charset"
+)
+
+// Username / realname / webirc hostname can all have configurable replacements
+func makeClientReplacements(format string, client *Client) string {
+	ret := format
+	ret = strings.Replace(ret, "%i", Ipv4ToHex(client.RemoteAddr), -1)
+	ret = strings.Replace(ret, "%h", client.RemoteHostname, -1)
+	return ret
+}
+
+func Ipv4ToHex(ip string) string {
+	var ipParts [4]int
+	fmt.Sscanf(ip, "%d.%d.%d.%d", &ipParts[0], &ipParts[1], &ipParts[2], &ipParts[3])
+	ipHex := fmt.Sprintf("%02x%02x%02x%02x", ipParts[0], ipParts[1], ipParts[2], ipParts[3])
+	return ipHex
+}
+
+func ensureUtf8(s string, fromEncoding string) string {
+	if utf8.ValidString(s) {
+		return s
+	}
+
+	encoding, encErr := charset.Lookup(fromEncoding)
+	if encoding == nil {
+		println("encErr:", encErr)
+		return ""
+	}
+
+	d := encoding.NewDecoder()
+	s2, _ := d.String(s)
+	return s2
+}
+
+func utf8ToOther(s string, toEncoding string) string {
+	if toEncoding == "UTF-8" && utf8.ValidString(s) {
+		return s
+	}
+
+	encoding, _ := charset.Lookup(toEncoding)
+	if encoding == nil {
+		return ""
+	}
+
+	e := encoding.NewEncoder()
+	s2, _ := e.String(s)
+	return s2
+}
