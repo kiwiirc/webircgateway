dca0adec81c06ea32e2f20de766f72f8fa76ccf4
ItsOnlyBinary <git@itsonlybinary.com>
2021-02-02 18:24:17 +0000

    Add recaptcha cache so multiple networks can use the same response

--- pkg/recaptcha/recaptcha.go
+++ pkg/recaptcha/recaptcha.go
@@ -7,8 +7,20 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
+
+var cacheVerified sync.Map
+var cacheLife, _ = time.ParseDuration("90s")
+var cacheFrequency, _ = time.ParseDuration("10m")
+var cacheTicker = time.NewTicker(cacheFrequency)
+
+// CacheItem represents an item in response cache
+type CacheItem struct {
+	created    int64
+	remoteAddr string
+}
 
 // R type represents an object of Recaptcha and has public property Secret,
 // which is secret obtained from google recaptcha tool admin interface
@@ -24,9 +36,55 @@ type googleResponse struct {
 	ErrorCodes []string `json:"error-codes"`
 }
 
+// Init starts recpatcha response cache cleanup
+func Init() {
+	go func() {
+		for range cacheTicker.C {
+			cleanCache()
+		}
+	}()
+}
+
+func cleanCache() {
+	cacheVerified.Range(func(key interface{}, value interface{}) bool {
+		cacheItem := value.(CacheItem)
+		expired := time.Now().Unix() - int64(cacheLife.Seconds())
+		if cacheItem.created < expired {
+			cacheVerified.Delete(key)
+		}
+		return true
+	})
+}
+
+func verifyCached(response string, remoteAddr string) bool {
+	cacheInterface, ok := cacheVerified.Load(response)
+	if !ok {
+		// Not in cache
+		return false
+	}
+	cacheItem := cacheInterface.(CacheItem)
+	if cacheItem.remoteAddr != remoteAddr {
+		// None matching remoteAddr
+		return false
+	}
+	expired := time.Now().Unix() - int64(cacheLife.Seconds())
+	if cacheItem.created < expired {
+		// Cached response expired
+		cacheVerified.Delete(remoteAddr)
+		return false
+	}
+
+	// Valid response
+	return true
+}
+
 // VerifyResponse is a method similar to `Verify`; but doesn't parse the form for you.  Useful if
 // you're receiving the data as a JSON object from a javascript app or similar.
-func (r *R) VerifyResponse(response string) bool {
+func (r *R) VerifyResponse(response string, remoteAddr string) bool {
+	if verifyCached(response, remoteAddr) {
+		// Response is cached and still valid
+		return true
+	}
 	r.lastError = make([]string, 1)
 	client := &http.Client{Timeout: 20 * time.Second}
 	resp, err := client.PostForm(r.URL,
@@ -47,7 +105,12 @@ func (r *R) VerifyResponse(response string) bool {
 		r.lastError = append(r.lastError, err.Error())
 		return false
 	}
-	if !gr.Success {
+	if gr.Success {
+		cacheVerified.Store(response, CacheItem{
+			created:    time.Now().Unix(),
+			remoteAddr: remoteAddr,
+		})
+	} else {
 		r.lastError = append(r.lastError, gr.ErrorCodes...)
 	}
 	return gr.Success
